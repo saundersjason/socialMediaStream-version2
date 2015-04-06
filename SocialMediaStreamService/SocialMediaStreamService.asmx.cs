6c998b7fca6e5b9b3eb1e14637c160c2e4788ff6
saundersjason <saundersj@savannahstate.edu>
2015-04-06 16:58:04 -0400

    New account

--- SocialMediaStreamService/SocialMediaStreamService.asmx.cs
+++ SocialMediaStreamService/SocialMediaStreamService.asmx.cs
@@ -23,95 +23,113 @@ namespace SavannahState
         private String _TwitterConsumerSecret = WebConfigurationManager.AppSettings["twitter_oauthconsumersecret"];
         private String _TwitterScreenName = WebConfigurationManager.AppSettings["twitter_screenname"];
 
+
+
+        //_FacebookAccessToken - 111f44b4b528cf408f9662ab629e39d8Reset
+
+        //Consumer Key (API Key)9gRaRlR5zAWPPQVXZ9P5xA
+        //Consumer Secret (API Secret)OJrKFw1sxnmTHOBWF3SoWtTbzjQDNP8w2sy90wy8
+        //Access Token87486291-4Wh2ZA5kCxsB68AhTIbUsTkIL4c6HyodsX8AODZE
+        //Access Token SecretK7cG41W4Wicemn2syKmSxmTjPbCA2JtheONAcfB6mP8
+
+
+
+
+
+
         public SocialMediaStreamService()
         {
-        }
-
-        [WebMethod(CacheDuration = 40)]
-        [ScriptMethod(UseHttpGet = true)]
-        public MediaItem GetAllSocialPostsFiltered(Int32 numberOfPosts, String mediaType, String hashTag, String facebookParams, String twitterParams, String instagramParams)
-        {
-            MediaItem filteredResult = new MediaItem();
-
-            filteredResult = GetAllSocialPosts(numberOfPosts, mediaType, facebookParams, twitterParams, instagramParams);
-            if (filteredResult.posts.Count > 0)
-            {
-                if (!String.IsNullOrEmpty(hashTag))
-                {
-                    filteredResult.posts = filteredResult.posts.Where(p => p.content.ToLower().Contains(hashTag.ToLower())).ToList();
-                }
-                
-                if (numberOfPosts > 0)
-                {
-                    filteredResult.posts = filteredResult.posts.OrderByDescending(o => o.postDate).Take(numberOfPosts).ToList();
-                }
-            }
-
-            Context.Response.AddHeader("Access-Control-Allow-Origin", "*");
-            Context.Response.AddHeader("Access-Control-Allow-Headers", "Content-Type");
-            return filteredResult;
         }
 
         [WebMethod]
         [ScriptMethod(UseHttpGet = true)]
         public MediaItem GetAllSocialPosts(Int32 numberOfPosts, String mediaType, String facebookParams, String twitterParams, String instagramParams)
         {
+
+
+
+
+
+            /*
+             NEXTURL's must be sent out in the same order as they are processed when dealing with multiple accounts from the same media service. If a nexturl is not available for a particular account, send a blank space or null as a placeholder.
+             */
+
+
+
+
+
             Result facebookResult=null;
             Result instagramResult=null;
             Result twitterResult=null;
+
+            MediaItem mediaItems = new MediaItem();
             if (mediaType == "instagram" || mediaType == "all")
             {
-                instagramResult = new InstagramRepository().GetFeed(_InstagramAccessToken, instagramParams, 20, 2, "", "", "", "");
+                if (!String.IsNullOrEmpty(instagramParams))
+                {
+                    String[] instagramParamArr = instagramParams.Split('|');
+                    foreach (String instagramParam in instagramParamArr)
+                    {
+                        instagramResult = new InstagramRepository().GetFeed(_InstagramAccessToken, instagramParam, 20, 2, "", "", "", "");
+                        if (instagramResult != null)
+                        {
+                            mediaItems.posts = mediaItems.posts.Concat(instagramResult.posts).ToList();
+                            if (!String.IsNullOrEmpty(instagramResult.NextUrl))
+                            {
+                                mediaItems.nextUrls.Add(new NextUrl { urlType = "instagram", url = instagramResult.NextUrl });
+                            }
+                        }
+                    }
+                }
             }
             if (mediaType == "facebook" || mediaType == "all")
             {
-                facebookResult = new FacebookRepository().GetFeed(_FacebookAccessToken, facebookParams, 20, 2, "", "", "", "");
+                if (!String.IsNullOrEmpty(facebookParams))
+                {
+                    String[] facebookParamArr = facebookParams.Split('|');
+                    foreach (String facebookParam in facebookParamArr)
+                    {
+                        facebookResult = new FacebookRepository().GetFeed(_FacebookAccessToken, facebookParam, 20, 2, "", "", "", "");
+                        if (facebookResult != null)
+                        {
+                            mediaItems.posts = mediaItems.posts.Concat(facebookResult.posts).ToList();
+                            if (!String.IsNullOrEmpty(facebookResult.NextUrl))
+                            {
+                                mediaItems.nextUrls.Add(new NextUrl { urlType = "facebook", url = facebookResult.NextUrl });
+                            }
+                        }
+                    }
+                }
             }
             if (mediaType == "twitter" || mediaType == "all")
             {
-                twitterResult = new TwitterRepository().GetFeed(_TwitterAccessToken, twitterParams, 20, 2, _TwitterAccessTokenSecret, _TwitterConsumerKey, _TwitterConsumerSecret, _TwitterScreenName);
+                if (!String.IsNullOrEmpty(twitterParams))
+                {
+                    String[] twitterParamArr = twitterParams.Split('|');
+                    foreach (String twitterParam in twitterParamArr)
+                    {
+                        twitterResult = new TwitterRepository().GetFeed(_TwitterAccessToken, twitterParam, 20, 2, _TwitterAccessTokenSecret, _TwitterConsumerKey, _TwitterConsumerSecret, _TwitterScreenName);
+                        if (twitterResult != null)
+                        {
+                            mediaItems.posts = mediaItems.posts.Concat(twitterResult.posts).ToList();
+                            if (!String.IsNullOrEmpty(twitterResult.NextUrl))
+                            {
+                                mediaItems.nextUrls.Add(new NextUrl { urlType = "twitter", url = twitterResult.NextUrl });
+                            }
+                        }
+                    }
+                }
             }
 
-            MediaItem mediaItems = new MediaItem();
-            if (instagramResult != null || facebookResult != null || twitterResult != null)
+            if (mediaItems.posts.Count > 0)
             {
-                if (instagramResult != null)
+                if (numberOfPosts > 0)
                 {
-                    mediaItems.posts = mediaItems.posts.Concat(instagramResult.posts).ToList();
-                    if (!String.IsNullOrEmpty(instagramResult.NextUrl))
-                    {
-                        mediaItems.nextUrls.Add(new NextUrl { urlType = "instagram", url = instagramResult.NextUrl });
-                    }
+                    mediaItems.posts = mediaItems.posts.OrderByDescending(o => o.postDate).Take(numberOfPosts).ToList();
                 }
-
-                if (facebookResult != null)
+                else
                 {
-                    mediaItems.posts = mediaItems.posts.Concat(facebookResult.posts).ToList();
-                    if (!String.IsNullOrEmpty(facebookResult.NextUrl))
-                    {
-                        mediaItems.nextUrls.Add(new NextUrl { urlType="facebook",url=facebookResult.NextUrl});
-                    }
-                }
-
-                if (twitterResult != null)
-                {
-                    mediaItems.posts = mediaItems.posts.Concat(twitterResult.posts).ToList();
-                    if (!String.IsNullOrEmpty(twitterResult.NextUrl))
-                    {
-                        mediaItems.nextUrls.Add(new NextUrl { urlType = "twitter", url = twitterResult.NextUrl });
-                    }
-                }
-
-                if (mediaItems.posts.Count > 0)
-                {
-                    if (numberOfPosts > 0)
-                    {
-                        mediaItems.posts = mediaItems.posts.OrderByDescending(o => o.postDate).Take(numberOfPosts).ToList();
-                    }
-                    else
-                    {
-                        mediaItems.posts = mediaItems.posts.OrderByDescending(o => o.postDate).ToList();
-                    }
+                    mediaItems.posts = mediaItems.posts.OrderByDescending(o => o.postDate).ToList();
                 }
             }
 
